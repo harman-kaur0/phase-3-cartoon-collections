cfc18677a62d99e0b5e88e99fdec1ef9cba96835
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-05-17 11:19:14 -0700

    commit tree 98ffd75b2b014919aa695f0d75703b350717b58e

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)# code an argument here
+  # Your code here
+  arr.each.with_index(1) {|e, index| puts "#{index}. #{e}"}
+end
+
+def summon_captain_planet(planeteer)# code an argument here
+  planeteer.collect {|e| "#{e.capitalize}!"}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(arr)# code an argument here
+  arr.any?{|i| i.size > 4}
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.detect{|e| cheese_types.include?(e)}
 end
